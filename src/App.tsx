69c249e1a8856b893a75a18dd0ae2f695245982a
o-henry <cksgod16@gmail.com>
2020-12-03 18:26:35 +0900

    ADD

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,54 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useReducer } from "react";
+import { nanoid } from "nanoid";
+
+function reducer(state: any, action: { type: any }) {
+  switch (action.type) {
+    case "ADD": {
+      return [
+        ...state,
+        {
+          id: nanoid(),
+          complted: false,
+        },
+      ];
+    }
+    default: {
+      return state;
+    }
+  }
+}
+
+function TodoItem({ id, completed }: any) {
+  return (
+    <>
+      <input type="checkbox" checked={completed} />
+      <input type="text" />
+      <button>Delete</button>
+    </>
+  );
+}
+
+function TodoList({ items }: any) {
+  return items.map((item: { id: string; completed: boolean }) => (
+    <TodoItem key={item.id} {...item} />
+  ));
+}
+
+function TodoApp() {
+  const [state, dispatch] = useReducer(reducer, []);
+
+  return (
+    <div>
+      <button onClick={() => dispatch({ type: "ADD" })}>ADD</button>
+      <TodoList items={state} />
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TodoApp />
     </div>
   );
 }
